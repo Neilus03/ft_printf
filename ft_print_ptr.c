8ed7d25fd895b6cd7ec7ff7e8fb15a3ab40c3130
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-04-24 10:28:09 +0200

    printf update

--- ft_print_ptr.c
+++ ft_print_ptr.c
@@ -6,16 +6,19 @@
 /*   By: nde-la-f <nde-la-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 09:32:04 by nde-la-f          #+#    #+#             */
-/*   Updated: 2023/04/20 11:41:01 by nde-la-f         ###   ########.fr       */
+/*   Updated: 2023/04/20 12:57:42 by nde-la-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void	ft_putchar_fd(char c, int fd)
+static void	ft_putchar_fd(char c, int fd, int *err)
 {
 	if (fd)
-		write (fd, &c, 1);
+	{	
+		if (write(fd, &c, 1) == -1)
+			*err = -1;
+	}
 }
 
 static int	ft_len_ptr(uintptr_t n)
@@ -33,27 +36,36 @@ static int	ft_len_ptr(uintptr_t n)
 	return (len);
 }
 
-static void	ft_putptr(uintptr_t n)
+static void	ft_putptr(uintptr_t n, int *err)
 {
 	if (n >= 16)
 	{	
-		ft_putptr(n / 16);
-		ft_putptr(n % 16);
+		ft_putptr(n / 16, err);
+		ft_putptr(n % 16, err);
 	}
 	else
 	{
 		if (n == 0)
-			write(1, "0", 1);
+		{
+			if (write(1, "0", 1) == -1)
+				*err = -1;
+		}
 		else if (n < 10)
-			ft_putchar_fd((n + '0'), 1);
+			ft_putchar_fd((n + '0'), 1, err);
 		else
-			ft_putchar_fd((n - 10 + 'a'), 1);
+			ft_putchar_fd((n - 10 + 'a'), 1, err);
 	}
 }
 
 int	ft_print_ptr(unsigned long long ptr)
 {
-	write(1, "0x", 2);
-	ft_putptr(ptr);
+	int	err;
+
+	err = 0;
+	if (write(1, "0x", 2) == -1)
+		return (-1);
+	ft_putptr(ptr, &err);
+	if (err == -1)
+		return (err);
 	return (2 + ft_len_ptr(ptr));
 }
--- ft_print_str.c
+++ ft_print_str.c
@@ -6,20 +6,24 @@
 /*   By: nde-la-f <nde-la-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 10:18:28 by nde-la-f          #+#    #+#             */
-/*   Updated: 2023/04/20 12:39:15 by nde-la-f         ###   ########.fr       */
+/*   Updated: 2023/04/20 12:47:50 by nde-la-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void	ft_putstr(char *str)
+static void	ft_putstr(char *str, int *len)
 {
 	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
-		write(1, &str[i], 1);
+		if (write(1, &str[i], 1) == -1)
+		{
+			*len = -1;
+			return ;
+		}
 		i++;
 	}
 }
@@ -30,15 +34,16 @@ int	ft_print_str(char *str)
 
 	len = 0;
 	if (!str)
-	{	
-		ft_putstr("(null)");
+	{
+		ft_putstr("(null)", &len);
 		if (len == -1)
 			return (-1);
 		return (6);
 	}
 	while (str[len])
 	{
-		write(1, &str[len], 1);
+		if (write(1, &str[len], 1) == -1)
+			return (-1);
 		len++;
 	}
 	return (len);
